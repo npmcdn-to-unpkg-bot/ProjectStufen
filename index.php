76095eca35024ff45fbcf1a7d9c2b81612c9c259
jhdv <jan.hakemann@me.com>
2016-05-15 14:18:34 +0200

    Bugfixes

--- index.php
+++ index.php
@@ -1,45 +1,53 @@
 <?php
 require_once "require/header.php";
 require_once "require/db.php";
-new MySQLDB("localhost", "root", "hBdapd223VPuazrmVK5Q", "stufen", "3306", "utf8") . connect();
-$mysql = MySQLDB::getInstance();
-$jobs = $mysql->getJobs();
+?>
 
-while ($row = $result->fetch_assoc()) {
+<h1>Test</h1>
 
-    $name = $row["name"];
-    $needed = $row["needed"];
-    $description = $row["description"];
-
-    $members = $mysql->getMembers($name);
-    $color = "";
-    if (mysqli_num_rows($members) >= $needed) {
-        $color = "grid-item color-green";
-    } else {
-        $color = "grid-item color-red";
-    }
-    echo '<div class="container">
+<div class="container">
     <div class="grid">
-        <div class="' . $color . '">
-            <h2>' . $name . '</h2>
-            <p>Braucht: ' . $needed . ' Personen</p>
-            <p>' . $description . '</p>
-            <hr>
-            <ul>';
-    while ($row2 = mysqli_fetch_assoc($members)) {
-        echo '<li>' . $row2["name"] . '</li>';
-    }
-    echo '</ul>
-            <hr>
-            <button class="btn btn -default btn - block" type="button" data-toggle="modal" data-target="#modal"
-                    data - job = "Job 1" > Eintragen <span class="glyphicon glyphicon-edit" aria - hidden = "true" ></span >
-            </button >
+        <?php
+        new MySQLDB("localhost", "root", "hBdapd223VPuazrmVK5Q", "stufen", "3306", "utf8") . connect();
+        $mysql = MySQLDB::getInstance();
+        $jobs = $mysql->getJobs();
+
+
+        while ($row = $jobs->fetch_assoc()) {
+
+            $name = $row["name"];
+            $needed = $row["needed"];
+            $description = $row["description"];
+
+            $members = $mysql->getMembers($name);
+            $color = "";
+            if (mysqli_num_rows($members) >= $needed) {
+                $color = "grid-item color-green";
+            } else {
+                $color = "grid-item color-red";
+            }
+            echo '<div class="' . $color . '">';
+            echo '<h2>' . $name . '</h2>';
+            echo '<p>Braucht: ' . $needed . ' Personen</p>';
+            echo '<p>' . $description . '</p>';
+            echo '<hr><ul>';
+            while ($row2 = $members->fetch_assoc()) {
+                echo '<li>' . $row2["name"] . '</li>';
+            }
+            echo '</ul><hr>';
+            echo '<button class="btn btn-default btn-block" type="button" data-toggle="modal" data-target="#modal"
+                    data-job="Job 1" > Eintragen <span class="glyphicon glyphicon-edit" aria-hidden="true" ></span >
+            </button >';'
         </div >
 
     </div >
 </div > ';
-}
-?>
+        }
+        ?>
+        ?>
+    </div>
+</div>
+
 
 <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modal">
     <div class="modal-dialog" role="document">
